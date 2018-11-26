523ff6d431238ebdddb334552bf9fd1ebf55c3d2
IvanPilyak <pilyak.vanya@gmail.com>
2018-11-26 22:44:29 +0200

    Modified TwentyThird.cs, ThirtyThird.cs.

--- ADO/ADO/Queries/ThirtyThird.cs
+++ ADO/ADO/Queries/ThirtyThird.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data;
 using System.Text;
-using System.Threading.Tasks;
+using ADO.Interfaces;
 
 namespace ADO.Queries
 {
-    class ThirtyThird
+    public class ThirtyThird : IQuery
     {
+        private readonly IDbConnection _connection;
+
+        public ThirtyThird(IDbConnection connection)
+        {
+            _connection = connection;
+            Title = "Change the City field in one of your records using the UPDATE statement";
+        }
+
+        public string Title { get; }
+
+        public string Execute()
+        {
+            var command = _connection.CreateCommand();
+            var stringBuilder = new StringBuilder();
+            command.CommandText = "UPDATE Employees " +
+                                  "SET City = 'Lexington' " +
+                                  "WHERE LastName = 'Mike' AND Notes LIKE 'Adam';";
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    stringBuilder.AppendFormat("{0}\t{1}\n", reader["FirstName"], reader["LastName"]);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
--- ADO/ADO/Queries/TwentyThird.cs
+++ ADO/ADO/Queries/TwentyThird.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Data;
 using System.Text;
-using System.Threading.Tasks;
+using ADO.Interfaces;
 
 namespace ADO.Queries
 {
-    class TwentyThird
+    public class TwentyThird : IQuery
     {
+        private readonly IDbConnection _connection;
+
+        public TwentyThird(IDbConnection connection)
+        {
+            _connection = connection;
+            Title = "Show the list of french customers’ names who used to order non-french products";
+        }
+
+        public string Title { get; }
+
+        public string Execute()
+        {
+            var command = _connection.CreateCommand();
+            var stringBuilder = new StringBuilder();
+            command.CommandText = "SELECT DISTINCT C.ContactName " +
+                                  "FROM Customers as C " +
+                                  "LEFT JOIN Orders AS O ON C.CustomerID = O.CustomerID " +
+                                  "LEFT JOIN [Order Details] AS OD ON O.OrderID = OD.OrderID " +
+                                  "LEFT JOIN [Products] AS P ON OD.ProductID = P.ProductID " +
+                                  "LEFT JOIN [Suppliers] AS S ON P.SupplierID = S.SupplierID " +
+                                  "WHERE S.Country <> 'France' AND C.Country = 'France';";
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    stringBuilder.AppendFormat("{0}\t{1}\n", reader["FirstName"], reader["LastName"]);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
